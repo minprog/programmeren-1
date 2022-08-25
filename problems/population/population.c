583112b7056711a5d01c78e2a91eeb8588fe07d3
jelleas <jelle.van.assema@gmail.com>
2022-08-25 14:15:13 +0200

    + population distro

---
+++ problems/population/population.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: prompt for start size
+
+    // TODO: prompt for end size
+
+    // TODO: calculate number of years until we reach threshold
+
+    // TODO: print number of years
+}
